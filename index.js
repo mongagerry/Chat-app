e87ea817ff32b385cdfa8862ab4be681a2daff9e
gerrymonga <mongagerry51@gmail.com>
2025-01-26 16:51:41 +0530

    change from esmodules to commonjs

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-import express from 'express'
+// import path from 'path'
+// import express from 'express'
+// import  url from 'url'
+// import http from 'http'
+// import socketio from 'socket.io'
+
+const path = require('path')
+const express = require('express')
+const url = require('url')
+const http = require('http')
+const socketio = require('socket.io')
+
+// const __filename = url.fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 3000
+
+io.on('connection', () =>{
+    console.log("A new WS connection")
+})
 
 app.listen(PORT, () =>{
     console.log("Server running on 3000")
